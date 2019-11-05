d9606c95f72cb359162d265f08355e09b8b69f9f
muzzammilfarooq <57043078+muzzammilfarooq@users.noreply.github.com>
2019-11-05 16:40:22 +0500

    sdfsdf

--- WindowsFormsApp/Form1.Designer.cs
+++ WindowsFormsApp/Form1.Designer.cs
@@ -63,9 +63,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(49, 86);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.Size = new System.Drawing.Size(67, 13);
             this.label1.TabIndex = 3;
-            this.label1.Text = "game345";
+            this.label1.Text = "asdfasfasdfa";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
@@ -73,9 +73,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(49, 124);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.Size = new System.Drawing.Size(28, 13);
             this.label2.TabIndex = 4;
-            this.label2.Text = "cituy";
+            this.label2.Text = "ttyyy";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // Form1
