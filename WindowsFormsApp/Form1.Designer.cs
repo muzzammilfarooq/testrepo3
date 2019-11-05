929dc741637df8bf9d45f37e42968e950be3e6fc
muzzammilfarooq <57043078+muzzammilfarooq@users.noreply.github.com>
2019-11-05 16:20:56 +0500

    game 345

--- WindowsFormsApp/Form1.Designer.cs
+++ WindowsFormsApp/Form1.Designer.cs
@@ -63,9 +63,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(49, 86);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.Size = new System.Drawing.Size(51, 13);
             this.label1.TabIndex = 3;
-            this.label1.Text = "newww";
+            this.label1.Text = "game345";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
